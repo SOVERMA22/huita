484304e01cc74addea7d7b8c9221cb7954e8e4e9
SOVERMA <abkbgg22>
2020-11-25 00:47:33 +0500

    pizdezz

---
+++ PCab.xaml.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Podgotovka
+{
+
+    public partial class PCab : Page
+    {
+        public PCab()
+        {
+            InitializeComponent();
+            using (TDE db = new TDE())
+            {
+                db.Users.Load();
+                CLV.ItemsSource = db.Users.Local.ToBindingList();                
+                Users user = db.Users.Where(U => U.id == MainMove.CurrentUserID).FirstOrDefault();
+                CabNum.Text = "Номер счета:" + user.number;
+                CabName.Text = user.Name;
+            }
+            
+            
+        }
+
+        private void Edit(object sender, RoutedEventArgs e)
+        {
+            using (TDE db = new TDE())
+            {
+                db.Users.Load();
+                var user = db.Users.Where(U => U.id == ((Users)CLV.SelectedValue).id).FirstOrDefault();
+                ETBID.Text = Convert.ToString(user.id);
+                ETBName.Text = user.Name;
+                ETBNum.Text = user.number;
+                EditPanel.Visibility = Visibility.Visible;
+                 
+            }
+        }
+
+        private void EditSave(object sender, RoutedEventArgs e)
+        {
+            using (TDE db = new TDE())
+            {
+                db.Users.Load();
+                var user = db.Users.Where(U => U.id == ((Users)CLV.SelectedValue).id).FirstOrDefault();
+                user.Name = ETBName.Text;
+                user.number = (ETBNum.Text);
+                db.SaveChanges();
+                CLV.Items.
+            }
+        }
+    }
+}
---
+++ PReg.xaml.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Podgotovka
+{
+
+    public partial class PReg : Page
+    {
+        public PReg()
+        {
+            InitializeComponent();
+            
+        }
+        TDE db = new TDE();
+
+        private void Registration(object sender, RoutedEventArgs e)
+        {
+            db.Users.Load();
+            using (TDE db = new TDE())
+            {
+
+                Users user = new Users()
+                {
+                    Name = RLTB.Text,
+                    number = RNTB.Text,
+                    Password = RPTB.Text                    
+                };
+                var newUserCheck = db.Users.Where(U => U.Name == RLTB.Text).FirstOrDefault();
+                if (newUserCheck == null)
+                {
+                    db.Users.Add(user);
+                    db.SaveChanges();
+                    MainMove.CurrentUserID = user.id;
+                    System.Windows.MessageBox.Show("Вы успешно зарегестрировались!");
+                    MainMove.MainFrame.Navigate(new PCab());
+                }
+                else System.Windows.MessageBox.Show("Такой пользователь уже зарегестрирован!");          
+
+                
+            }
+        }
+
+        private void Autorisation(object sender, RoutedEventArgs e)
+        {
+            db.Users.Load();
+            using (TDE db = new TDE())
+            {
+                var autoUserCheck = db.Users.Where(U => U.Name == ALTB.Text).FirstOrDefault();
+                if (autoUserCheck != null)
+                {
+                    MainMove.CurrentUserID = autoUserCheck.id;
+                    System.Windows.MessageBox.Show("Вы успешно авторизовались!");
+                    MainMove.MainFrame.Navigate(new PCab());
+                }
+            }
+            
+        }
+    }
+}
